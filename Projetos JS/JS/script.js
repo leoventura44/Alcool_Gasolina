1197517ac8949f2f8985f0969b93517b8ed154e3
Leonardo P. Ventura <109251881+leoventura44@users.noreply.github.com>
2022-08-31 19:46:05 -0300

    Add files via upload

---
+++ Projetos JS/JS/script.js
@@ -0,0 +1,25 @@
+function calcularMelhorPreco() {
+
+        let precoAlcool = document.getElementById('alcool').value
+        let precoGasolina = document.getElementById('gasolina').value
+
+        if(precoAlcool != '') {
+          if (precoGasolina != '') {
+
+            let resultado = precoAlcool / precoGasolina
+            if( resultado >= 0.7 ) {
+              
+              document.getElementById('resultado').innerHTML = 'Melhor utilizar Gasolina'
+            }else{
+              
+              document.getElementById('resultado').innerHTML = 'Melhor utilizar Álcool'
+            }
+
+         }else{
+          alert('Insira o valor da Gasolina')
+         }
+        }else{
+          alert('Insira o valor do Álcool')
+        }
+
+      }
